460928f0f45dedf4ca1c7c7ec37378d324cde701
DuyLeHong <leduy.av22@gmail.com>
2021-08-05 12:08:25 +0700

    commit change

--- buoi9/buoi9Demospringboot/src/main/java/com/codefresher/buoi9/Buoi9DemospringbootApplication.java
+++ buoi9/buoi9Demospringboot/src/main/java/com/codefresher/buoi9/Buoi9DemospringbootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Buoi9DemospringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Buoi9DemospringbootApplication.class, args);
+		SpringApplication.run(Buoi9DemospringbootApplication.class, args); // check
 	}
 
 }
